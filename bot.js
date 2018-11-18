3db2891600a288b5c410108749c2328192aca74e
Anthony DiVanna <ajdiv93@gmail.com>
2018-11-18 14:15:11 -0500

    Test Change

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ function respond() {
 function postMessage() {
   var botResponse, options, body, botReq;
 
-  botResponse = cool();
+  botResponse = cool() + 'test';
 
   options = {
     hostname: 'api.groupme.com',
