13ae5a971d6bcd2b9fb7cb6d588ee480ac3d6df7
Anthony DiVanna <ajdiv93@gmail.com>
2018-11-19 21:01:32 -0500

    Added sample code with callbacks to test bot responses

--- bot.js
+++ bot.js
@@ -4,12 +4,22 @@ var cool = require('cool-ascii-faces');
 var botID = process.env.BOT_ID;
 
 function respond() {
-  var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^\/cool guy$/;
+  var request = JSON.parse(this.req.chunks[0]);
+  var coolGuyRegex = /^\/cool guy$/;
+  var thesaurusRegex = /^\/thesaurize$/;
 
-  if(request.text && botRegex.test(request.text)) {
+  var response = this.res;
+
+  if(request.text && coolGuyRegex.test(request.text)) {
     this.res.writeHead(200);
-    postMessage();
+    postCoolGuyMessage(function(results) {
+      response.end(results);
+    });
+  } else if (request.text && thesaurusRegex.test(request.text)){
+    this.res.writeHead(200);
+    postThesaurizeMessage(function(results){
+      response.end(results);
+    });
     this.res.end();
   } else {
     console.log("don't care");
@@ -18,10 +28,47 @@ function respond() {
   }
 }
 
-function postMessage() {
+function postCoolGuyMessage(callback) {
   var botResponse, options, body, botReq;
 
-  botResponse = cool() + 'test';
+  botResponse = cool() + 'COOL';
+
+  options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+  };
+
+  body = {
+    "bot_id" : botID,
+    "text" : botResponse
+  };
+
+  console.log('sending ' + botResponse + ' to ' + botID);
+
+  botReq = HTTPS.request(options, function(res) {
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  var results = JSON.stringify(body);
+  botReq.end(results);
+  return callback(results);
+}
+
+function postThesaurizeMessage(callback) {
+  var botResponse, options, body, botReq;
+
+  botResponse =  cool() + 'THESAURUS';
 
   options = {
     hostname: 'api.groupme.com',
@@ -51,6 +98,9 @@ function postMessage() {
     console.log('timeout posting message '  + JSON.stringify(err));
   });
   botReq.end(JSON.stringify(body));
+  var results = JSON.stringify(body);
+  botReq.end(results);
+  return callback(results);
 }
 
 
