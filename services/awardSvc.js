46d419943a3c5dbe99eb645d15a80dff23a0f151
your username <jaddamo513@yahoo.com>
2019-06-27 20:03:57 -0400

    test

---
+++ services/awardSvc.js
@@ -0,0 +1,12 @@
+function addMsgCounter(){
+    var counter = 0;            //Initialize counter 
+    counter += 1;
+    return counter;
+}
+
+
+
+/* var counter = 0
+function addMsg(){
+    counter += 1;
+}*/
