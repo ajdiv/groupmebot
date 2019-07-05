75f7dd6a1ced244a5b7c6054a41949758ac1b43e
your username <jaddamo513@yahoo.com>
2019-07-05 15:36:41 -0400

    test

--- services/awardSvc.js
+++ services/awardSvc.js
@@ -4,9 +4,56 @@ function addMsgCounter(){
     return counter;
 }
 
+//var MongoClient = require('').MongoClient;
+//var url = "";
 
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-/* var counter = 0
-function addMsg(){
-    counter += 1;
-}*/
+let DailyUserPostCounterSchema = new Schema({
+    gmeUserId: {type: Number, required: true},
+    gmeGroupId: {type: Number, required: true},
+    messageCount: {type: Number, required: true},
+    date: {type: Date, required: true},
+});
+
+var User =   mongoose.model('DailyUserPostCounter', DailyUserPostCounterSchema);
+// Export the model
+module.exports = User;
+
+/* look up mongo syntax,
+
+groupmeuserid: , date: ,
+
+*/
+
+// get user
+function getUser(){
+    return this.User;
+}
+
+//sort and rank
+var array = []
+function swap(array, i, j){
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+}
+function sSort(array){
+    for(var i = 0; i < array.length; i++){
+        var min = i;
+        for(var j = i + 1; j < array.length; j++){
+            if(array[j] < array[min]){
+                min = j;
+            }
+        }
+    }
+    if(i !== min){
+        swap(array, i, min);
+    }
+}
+return array;
+
+function rankUser(){
+    var 
+}
