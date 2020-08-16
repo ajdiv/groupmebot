ed38724b6eda43e1306dda47d002b1e88275939d
Anthony DiVanna <anthony.divanna@gmail.com>
2020-08-16 15:43:35 -0400

    Full unit testing for bot tool

--- src/bot/botTool.spec.ts
+++ src/bot/botTool.spec.ts
@@ -1,3 +1,7 @@
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
+import chai, { expect } from 'chai';
+import chaiAsPromised from 'chai-as-promised';
 import { beforeEach, describe, it } from 'mocha';
 import sinon from 'sinon';
 import { CommandFactory } from '../commands/commandFactory';
@@ -5,50 +9,117 @@ import { CommandCheckLocation } from '../commands/constants/commandCheckLocation
 import { Command } from '../commands/models/command';
 import { SenderTypes } from '../groupme/constants/senderTypes';
 import { Message } from '../groupme/models/message';
+import { BotResponse } from './botResponse';
 import { BotTool } from './botTool';
 
+chai.use(chaiAsPromised);
+
 describe('Bot Tool Tests', () => {
-  let messageStub: Message;
-  let commandStub: Command;
+  const postUrl = 'https://api.groupme.com/v3/bots/post';
+
+  // Custom input/outputs we're configuring ourselves
+  let testMessage: Message;
+  let testCommand: Command;
+  let testCommandResult: BotResponse;
+
+  // Spies/stubs
+  let axiosStub: MockAdapter = new MockAdapter(axios);
   let getCommandStub = sinon.stub(CommandFactory, 'getCommand');
-  let sendBotResponseSpy = sinon.stub(BotTool, <any>'sendBotResponse');
+  let axiosSpy = sinon.spy(axios, 'post');
 
   beforeEach(() => {
-    messageStub = new Message();
-
-    commandStub = {
+    // Create "fresh" instances of our custom inputs/outputs
+    testMessage = new Message();
+    testCommandResult = new BotResponse('Test Result');
+    testCommand = {
       commandText: ['/test'],
       commandCheckLocation: CommandCheckLocation.Start,
       helpText: 'For testing',
-      execute: null};
+      execute: () => {
+        return Promise.resolve(testCommandResult)
+      }
+    };
+
+    //axiosStub = new MockAdapter(axios);
   })
 
-  it('should send nothing if the message sent was from a Bot', () => {
-    messageStub.sender_type = SenderTypes.Bot;
+  afterEach(() => {
+    // Clean out our spies/stubs
+    axiosStub.reset();
+    getCommandStub.resetHistory();
+    axiosSpy.resetHistory();
+  })
 
-    BotTool.readMessageAndRespond(messageStub);
+  it('should send nothing if no input was given', async () => {
+    // Arrange
+    testMessage = null;
 
+    // Act
+    await BotTool.readMessageAndRespond(testMessage);
+
+    // Assert
     sinon.assert.notCalled(getCommandStub);
-    sinon.assert.notCalled(sendBotResponseSpy);
+    sinon.assert.notCalled(axiosSpy);
   });
 
-  it('should send nothing if the message sent had no text in it', () => {
-    messageStub.text = '';
 
-    BotTool.readMessageAndRespond(messageStub);
+  it('should send nothing if the message sent was from a Bot', async () => {
+    // Arrange
+    testMessage.sender_type = SenderTypes.Bot;
 
+    // Act
+    await BotTool.readMessageAndRespond(testMessage);
+
+    // Assert
     sinon.assert.notCalled(getCommandStub);
+    sinon.assert.notCalled(axiosSpy);
   });
 
-  it('should send nothing if the message contained no actionable command', () => {
-    messageStub.text = '/test';
+  it('should send nothing if the message sent had no text in it', async () => {
+    // Arrange
+    testMessage.text = '';
 
+    // Act
+    await BotTool.readMessageAndRespond(testMessage);
+
+    // Assert
+    sinon.assert.notCalled(getCommandStub);
+    sinon.assert.notCalled(axiosSpy);
+  });
+
+  it('should send nothing if the message contained no actionable command', async () => {
+    // Arrange
+    testMessage.text = '/test';
     getCommandStub.returns(null);
-    sendBotResponseSpy.returns(null);
 
-    BotTool.readMessageAndRespond(messageStub);
+    // Act
+    await BotTool.readMessageAndRespond(testMessage);
 
+    // Assert
     sinon.assert.calledOnce(getCommandStub);
-    sinon.assert.notCalled(sendBotResponseSpy);
+  });
+
+  it('should send a POST request to GroupMe and return command result when successful', async () => {
+    // Arrange
+    testMessage.text = '/test';
+    getCommandStub.returns(testCommand);
+    axiosStub.onPost(postUrl).reply(200);
+
+    // Act
+    let results = await BotTool.readMessageAndRespond(testMessage);
+
+    // Assert
+    sinon.assert.calledOnce(axiosSpy);
+    expect(results).to.equal(testCommandResult.text)
+  });
+
+  it('should throw an error when there is an error sending the message to GroupMe service', async () => {
+    // Arrange
+    testMessage.text = '/test';
+    getCommandStub.returns(testCommand);
+    axiosStub.onPost(postUrl).networkError();
+
+    // Act (& Assert)
+    await expect(BotTool.readMessageAndRespond(testMessage)).to.eventually.be.rejectedWith(Error);
   });
 });
--- src/bot/botTool.ts
+++ src/bot/botTool.ts
@@ -1,4 +1,4 @@
-import { request, RequestOptions } from 'https';
+import axios from 'axios';
 import { CommandFactory } from '../commands/commandFactory';
 import { SenderTypes } from '../groupme/constants/senderTypes';
 import { Message } from '../groupme/models/message';
@@ -7,12 +7,12 @@ import { BotResponse } from './botResponse';
 
 export abstract class BotTool {
 
-  private static readonly GROUPME_HOSTNAME: string = 'api.groupme.com';
+  private static readonly GROUPME_HOSTNAME: string = 'https://api.groupme.com';
   private static readonly GROUPME_PATH: string = '/v3/bots/post';
 
   static async readMessageAndRespond(reqBody: Message): Promise<string> {
     // Don't respond to other bots or empty images
-    if (reqBody.sender_type === SenderTypes.Bot || !reqBody.text) return 'No text to read';
+    if (!reqBody || reqBody.sender_type === SenderTypes.Bot || !reqBody.text) return 'No text to read';
 
     let cleanText = reqBody.text.trim().toLowerCase();
     const command = CommandFactory.getCommand(cleanText);
@@ -31,26 +31,8 @@ export abstract class BotTool {
       console.log(error);
     }
 
-    this.sendBotResponse(results);
+    await axios.post(this.GROUPME_HOSTNAME + this.GROUPME_PATH, results);
+
     return responseMsg;
   }
-
-  private static sendBotResponse(responseModel: BotResponse) {
-    const reqOptions: RequestOptions = {
-      hostname: this.GROUPME_HOSTNAME,
-      path: this.GROUPME_PATH,
-      method: 'POST'
-    };
-
-    const clientReq = request(reqOptions);
-
-    clientReq.on('error', (err: any) => {
-      console.log('Error posting message to GroupMe: ' + JSON.stringify(err));
-    });
-    clientReq.on('timeout', function (err: any) {
-      console.log('Timeout posting message to GroupMe: ' + JSON.stringify(err));
-    });
-
-    clientReq.end(JSON.stringify(responseModel));
-  };
 }
